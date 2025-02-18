b4fd50b8f743dc85665ad5085af33f4326e4424b
Ryan Wright <wright08@vt.edu>
2025-02-18 13:01:01 -0800

    use defined muxer

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 // token based bearer auth jwt
 func authUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { // implicit handler conversion
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("authyy")
 		// open up a connection to the user db
 		// if bearer doesnt match user creds, die
@@ -27,20 +27,21 @@ func getImage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	addr := "localhost:8080"
+	mux := http.NewServeMux()
 
 	//user
-	http.HandleFunc("POST /users/{username}/images", uploadImage)
-	http.HandleFunc("GET /users/{username}/images", listImages)
-	http.HandleFunc("PUT /users/{username}/images/{image_id}/activate", activateImage)
-	http.HandleFunc("DELETE /users/{username}/images/{image_id}", delImage)
+	mux.HandleFunc("POST /users/{username}/images", uploadImage)
+	mux.HandleFunc("GET /users/{username}/images", listImages)
+	mux.HandleFunc("PUT /users/{username}/images/{image_id}/activate", activateImage)
+	mux.HandleFunc("DELETE /users/{username}/images/{image_id}", delImage)
+	// func: create user
 	// func: del user
 
 	//public
-	http.HandleFunc("GET /{username}", getImage)
-	// func: create user
+	mux.HandleFunc("GET /{username}", getImage)
 
 	// middlware
-	handler := authUser(http.DefaultServeMux)
+	handler := authUser(mux)
 
 	log.Printf("listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, handler))
